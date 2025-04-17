d6e21f9a858cdd7e02d27f3b41991b316b5bee1c
Joshua <73680704+BarcodeBimbo@users.noreply.github.com>
2025-04-17 01:36:45 -0400

    Update AssemblyInfo.cs

--- Update 1.0.2/Smokey/AssemblyInfo.cs
+++ Update 1.0.2/Smokey/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System;
 
 [assembly: Guid("ae79991d-d250-4aa7-bb68-7c13ae8bbfd1")]
 
-[assembly: MelonInfo(typeof(HelloDiddy.MainMod), "GoonerSquad", "1.0.1", "HelloDiddy x GoonerSquad")]
+[assembly: MelonInfo(typeof(HelloDiddy.MainMod), "GoonerSquad", "1.0.2", "HelloDiddy x GoonerSquad")]
 [assembly: MelonGame("TVGS", "Schedule I")]
 [assembly: MelonColor(255, 192, 211, 255)]
 
